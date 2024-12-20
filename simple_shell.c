168f958ebc978a0db6531c2fd212357a44e85c06
Leyla0711 <lsuleymanova.ns@gmail.com>
2024-12-20 16:59:38 +0000

    changed everything

--- simple_shell.c
+++ simple_shell.c
@@ -1,42 +1,83 @@
-#include "main.h"
-#include <unistd.h>  /* For execve */
-#include <sys/wait.h>  /* For waitpid */
-#include <stdlib.h>  /* For malloc */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
 
-/* Declare environ globally */
+#define PROMPT "#cisfun$ "
+
+/* Extern declaration for the environment variable */
 extern char **environ;
 
 /**
- * run_shell - Function to run the shell
- * @command: The command to execute
+ * main - Simple Shell main function
+ *
+ * Return: 0 on success
  */
-void run_shell(char *command)
+int main(void)
 {
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
     pid_t pid;
     int status;
-    char *args[2];  /* Array to hold command and its arguments */
 
-    /* Allocate memory for args dynamically */
-    args[0] = command;
-    args[1] = NULL;
-
-    pid = fork();
-    if (pid == 0) /* Child process */
+    while (1)
     {
-        if (execve(command, args, environ) == -1) /* Execute command */
+        /* Display prompt */
+        printf(PROMPT);
+        fflush(stdout);
+
+        /* Read input from user */
+        nread = getline(&line, &len, stdin);
+
+        /* Handle EOF (Ctrl+D) */
+        if (nread == -1)
         {
-            perror("Error executing command");
+            if (feof(stdin))  /* Check for EOF */
+            {
+                free(line);
+                printf("\n");
+                exit(0);
+            }
+            else
+            {
+                perror("getline");
+                exit(1);
+            }
+        }
+
+        /* Remove trailing newline character */
+        if (line[nread - 1] == '\n')
+            line[nread - 1] = '\0';
+
+        /* Create child process to execute command */
+        pid = fork();
+        if (pid == -1)
+        {
+            perror("fork");
             exit(1);
         }
+        else if (pid == 0) /* Child process */
+        {
+            /* Execute command using execve */
+            if (execve(line, (char *[]) {line, NULL}, environ) == -1)
+            {
+                /* If execve fails, print error message */
+                perror("./shell");
+                exit(1);
+            }
+        }
+        else /* Parent process */
+        {
+            /* Wait for the child process to terminate */
+            waitpid(pid, &status, 0);
+        }
     }
-    else if (pid < 0) /* Fork failed */
-    {
-        perror("Fork failed");
-        exit(1);
-    }
-    else /* Parent process */
-    {
-        waitpid(pid, &status, 0); /* Wait for child process to terminate */
-    }
-}
 
+    /* Free memory and exit */
+    free(line);
+    return 0;
+}
