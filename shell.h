761e8c76b0ba33b6d5abf0af68b84dfdedf9f83a
Leyla0711 <lsuleymanova.ns@gmail.com>
2024-12-23 10:50:10 +0000

    answears

--- shell.h
+++ shell.h
@@ -3,14 +3,34 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
 
-void shell_loop(void);  /* Declare shell_loop */
-char *read_input(void);  /* Correct declaration for read_input */
-int execute_command(char **args);  /* Declaration for command execution */
+
+int lsh_ctrld(char **args);
+int lsh_cd(char **args);
+int lsh_help(char **args);
+extern char **environ;
+int lsh_exit(char **args);
+int _strcmp(char *s1, char *s2);
+size_t _strncmp(char *s1, char *s2, size_t n);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+int _putchar(char c);
+
+char *_get_path(char **env);
+int _values_path(char **arg, char **env);
+char *_getline_command(void);
+void _getenv(char **env);
+char **tokenize(char *lineptr);
+void _exit_command(char **args, char *lineptr, int _exit);
+int _fork_fun(char **arg, char **av, char **env,
+char *lineptr, int np, int c);
+
 
 #endif /* SHELL_H */
-
